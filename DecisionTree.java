fe4954b3bc250a1c6a300598a5c7f4031576ef4b
BlurplePanda <bootham@wgc.school.nz>
2023-09-09 22:45:39 +1200

    comments/format

--- DecisionTree.java
+++ DecisionTree.java
@@ -11,23 +11,23 @@
 /**
  * Implements a decision tree that asks a user yes/no questions to determine a decision.
  * Eg, asks about properties of an animal to determine the type of animal.
- * 
- * A decision tree is a tree in which all the internal nodes have a question, 
+ * <p>
+ * A decision tree is a tree in which all the internal nodes have a question,
  * The answer to the question determines which way the program will
- *  proceed down the tree.  
+ * proceed down the tree.
  * All the leaf nodes have the decision (the kind of animal in the example tree).
- *
+ * <p>
  * The decision tree may be a predetermined decision tree, or it can be a "growing"
  * decision tree, where the user can add questions and decisions to the tree whenever
  * the tree gives a wrong answer.
- *
+ * <p>
  * In the growing version, when the program guesses wrong, it asks the player
  * for another question that would help it in the future, and adds it (with the
- * correct answers) to the decision tree. 
- *
+ * correct answers) to the decision tree.
  */
 
 import ecs100.*;
+
 import java.util.*;
 import java.io.*;
 import java.nio.file.*;
@@ -36,6 +36,7 @@ import java.awt.Color;
 public class DecisionTree {
 
     public DTNode theTree;    // root of the decision tree;
+
     public enum Branch {
         yes, no
     }
@@ -43,7 +44,7 @@ public class DecisionTree {
     /**
      * Setup the GUI and make a sample tree
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         DecisionTree dt = new DecisionTree();
         dt.setupGUI();
         dt.loadTree("sample-animal-tree.txt");
@@ -52,30 +53,34 @@ public class DecisionTree {
     /**
      * Set up the interface
      */
-    public void setupGUI(){
-        UI.addButton("Load Tree", ()->{loadTree(UIFileChooser.open("File with a Decision Tree"));});
+    public void setupGUI() {
+        UI.addButton("Load Tree", () -> {
+            loadTree(UIFileChooser.open("File with a Decision Tree"));
+        });
         UI.addButton("Print Tree", this::printTree);
         UI.addButton("Run Tree", this::runTree);
         UI.addButton("Grow Tree", this::growTree);
         // UI.addButton("Save Tree", this::saveTree);  // for completion
         // UI.addButton("Draw Tree", this::drawTree);  // for challenge
-        UI.addButton("Reset", ()->{loadTree("sample-animal-tree.txt");});
+        UI.addButton("Reset", () -> {
+            loadTree("sample-animal-tree.txt");
+        });
         UI.addButton("Quit", UI::quit);
         UI.setDivider(0.5);
     }
 
-    /**  
+    /**
      * Print out the contents of the decision tree in the text pane.
      * The root node should be at the top, followed by its "yes" subtree,
      * and then its "no" subtree.
      * Needs a recursive "helper method" which is passed a node.
-     * 
+     *
      * COMPLETION:
      * Each node should be indented by how deep it is in the tree.
      * The recursive "helper method" is passed a node and an indentation string.
      *  (The indentation string will be a string of space characters)
      */
-    public void printTree(){
+    public void printTree() {
         UI.clearText();
         UI.println(nodeText(theTree));
         printSubTree(theTree.getYes(), Branch.yes, 1);
@@ -90,10 +95,12 @@ public class DecisionTree {
      * @param branch enum to represent if it is in the "yes" or "no" branch of the previous node
      */
     public void printSubTree(DTNode node, Branch branch, int layer) {
-        if (node == null) { return; }
-        UI.print((branch.name()+":"+nodeText(node)).indent(layer*4));
-        printSubTree(node.getYes(), Branch.yes, layer+1);
-        printSubTree(node.getNo(), Branch.no, layer+1);
+        if (node == null) {
+            return;
+        }
+        UI.print((branch.name() + ":" + nodeText(node)).indent(layer * 4));
+        printSubTree(node.getYes(), Branch.yes, layer + 1);
+        printSubTree(node.getNo(), Branch.no, layer + 1);
     }
 
     /**
@@ -102,7 +109,7 @@ public class DecisionTree {
      * @return the node text with a question mark at the end if it is not an answer
      */
     public String nodeText(DTNode node) {
-        return node.getText()+(!node.isAnswer()?"?":"");
+        return node.getText() + (!node.isAnswer() ? "?" : "");
     }
 
     /**
@@ -117,9 +124,16 @@ public class DecisionTree {
         runSubTree(theTree);
     }
 
+    /**
+     * helper method for runTree and growTree
+     * runs through a subtree until it reaches a leaf node
+     * prints and returns the leaf node
+     * @param node the "root" of the subtree
+     * @return the leaf node it ends at
+     */
     public DTNode runSubTree(DTNode node) {
         if (node.isAnswer()) {
-            UI.println("The answer is: "+node.getText());
+            UI.println("The answer is: " + node.getText());
             return node;
         }
         boolean yes = UI.askBoolean("Is it true: " + node.getText() + " (Y/N): ");
@@ -143,14 +157,14 @@ public class DecisionTree {
      *  - changes the text in the node to be the question
      *  - adds two new children (leaf nodes) to the node with the two decisions.
      */
-    public void growTree () {
-        DTNode leaf = runSubTree(theTree);
+    public void growTree() {
+        DTNode leaf = runSubTree(theTree); // the leaf node it ends up at
         boolean correctAnswer = UI.askBoolean("Is that right? ");
         if (!correctAnswer) {
             String actualAnswer = UI.askString("OK, what should the answer be? ");
-            String property = UI.askString("Oh. I can't distinguish a "+leaf.getText()+" from a "
-                    +actualAnswer+"\n"+"Tell me something that's true for a "+actualAnswer+" but not for a "
-                    +leaf.getText()+"?\n"+"Property: ");
+            String property = UI.askString("Oh. I can't distinguish a " + leaf.getText() + " from a "
+                    + actualAnswer + "\n" + "Tell me something that's true for a " + actualAnswer + " but not for a "
+                    + leaf.getText() + "?\n" + "Property: ");
             DTNode answerYes = new DTNode(actualAnswer);
             DTNode answerNo = new DTNode(leaf.getText());
             leaf.setText(property);
@@ -164,19 +178,22 @@ public class DecisionTree {
 
     // Written for you
 
-    /** 
+    /**
      * Loads a decision tree from a file.
      * Each line starts with either "Question:" or "Answer:" and is followed by the text
      * Calls a recursive method to load the tree and return the root node,
      *  and assigns this node to theTree.
      */
-    public void loadTree (String filename) { 
-        if (!Files.exists(Path.of(filename))){
-            UI.println("No such file: "+filename);
+    public void loadTree(String filename) {
+        if (!Files.exists(Path.of(filename))) {
+            UI.println("No such file: " + filename);
             return;
         }
-        try{theTree = loadSubTree(new ArrayDeque<String>(Files.readAllLines(Path.of(filename))));}
-        catch(IOException e){UI.println("File reading failed: " + e);}
+        try {
+            theTree = loadSubTree(new ArrayDeque<String>(Files.readAllLines(Path.of(filename))));
+        } catch (IOException e) {
+            UI.println("File reading failed: " + e);
+        }
     }
 
     /**
@@ -187,12 +204,12 @@ public class DecisionTree {
      *    from the scanner and add them as the  children of the node,
      * Finally, it should return the  node.
      */
-    public DTNode loadSubTree(Queue<String> lines){
+    public DTNode loadSubTree(Queue<String> lines) {
         Scanner line = new Scanner(lines.poll());
         String type = line.next();
         String text = line.nextLine().trim();
         DTNode node = new DTNode(text);
-        if (type.equals("Question:")){
+        if (type.equals("Question:")) {
             DTNode yesCh = loadSubTree(lines);
             DTNode noCh = loadSubTree(lines);
             node.setChildren(yesCh, noCh);
@@ -200,7 +217,6 @@ public class DecisionTree {
         return node;
 
     }
-
 
 
 }
